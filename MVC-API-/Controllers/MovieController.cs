c202fc0673fa223054d6a572c3539562ae5421c3
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-08-21 18:05:37 +0300

    added mvc

---
+++ MVC-API-/Controllers/MovieController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_API_.Models;
+using System.Net.Http;
+
+namespace MVC_API_.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly HttpClient httpclient;
+
+        public MovieController(IHttpClientFactory factory)
+        {
+            httpclient = factory.CreateClient("API-Assignment_1-");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<Movie> movies = await httpclient.GetFromJsonAsync<List<Movie>>("Movie");
+            return View(movies);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById(int Id)
+        {
+            Movie movie = await httpclient.GetFromJsonAsync<Movie>($"Movie/{Id}");
+            return View("Details",movie);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Movie movie)
+        {
+            var create = await httpclient.PostAsJsonAsync("Movie", movie);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int Id)
+        {
+            var movie = await httpclient.GetFromJsonAsync<Movie>($"Movie/{Id}");
+            return View(movie);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Movie movie)
+        {
+            var update = await httpclient.PutAsJsonAsync("Movie", movie);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var movie = await httpclient.GetFromJsonAsync<Movie>($"Movie/{Id}");
+            return View(movie);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveDelete(int id)
+        {
+            var response = await httpclient.DeleteAsync($"Movie/{id}");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC-API-/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_API_.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string ReleaseYear { get; set; }
+    }
+}
